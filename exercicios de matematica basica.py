ddf274687f6604c18aefad9763bc99342e805183
Giovanna Albertini <67559787+giovannaalbertini@users.noreply.github.com>
2023-04-18 10:37:50 -0300

    Update exercicios de matematica basica.py

--- exercicios de matematica basica.py
+++ exercicios de matematica basica.py
@@ -113,26 +113,3 @@ else:
     x1 = (b + np.sqrt(delta))/ (2*a)
     x2 = (b - np.sqrt(delta))/ (2*a)
     print(f"As raizes da equacao sao: x1= {x1} e x2 = {x2}")
-    
-#calculadora de impostos com base no salario e na tabela de impostos do governo
-
-salario = float(input('digite seu salario: '))
-
-if salario <= 1903.99:
-   print ('voce esta isento do imposto de renda!')
-
-elif salario >= 1903.99 and salario <= 2826.65:
-  salario_atual = salario -(0.075 * salario) + 142
-  print ('seu salario apos desconto sera de: ', salario_atual)
-  
-elif salario >= 2826.66 and salario <= 3751.05:
-  salario_atual = salario - (0.15 * salario) + 354.80
-  print ('seu salario apos desconto sera de: ', salario_atual)
-  
-elif salario >= 3751.06 and salario <= 4664.68:
-  salario_atual = salario - (0.225 * salario) + 636.13
-  print ('seu salario apos desconto sera de: ', salario_atual)
-  
-elif salario >= 4664.69:
-  salario_atual = salario - (0.275 * salario) + 869.36  
-print ('seu salario apos desconto sera de: ', salario_atual)
